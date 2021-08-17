faecad060ed916f5046bdbd93e015356926c0b5f
gitNermin <shawkat.nermin@gmail.com>
2021-08-17 02:34:47 +0200

    Move Crossection with mouse

--- Assets/_PaperCurl/Scripts/Turner.cs
+++ Assets/_PaperCurl/Scripts/Turner.cs
@@ -6,12 +6,25 @@ public class Turner : MonoBehaviour
 {
     Vector3 _startPosition;
     Material _myMaterial;
-    Rect _rect;
+
+    [SerializeField]
+    float _width;
+    [SerializeField]
+    float _height;
+    [SerializeField]
+    float _deltaDistance = 0.1f;
+
+    float _distance = 0;
+
+    Vector3[] _corners = new Vector3[4];
 
     private void Start()
     {
         _myMaterial = GetComponent<Renderer>().material;
-        _rect = new Rect();
+        _corners[0] = transform.position + Vector3.right * _width / 2f + Vector3.up * _height / 2;
+        _corners[1] = transform.position + Vector3.left * _width / 2f + Vector3.up * _height / 2;
+        _corners[2] = transform.position + Vector3.right * _width / 2f + Vector3.down * _height / 2;
+        _corners[3] = transform.position + Vector3.left * _width / 2f + Vector3.down * _height / 2;
     }
 
     private void OnMouseDown()
@@ -19,11 +32,28 @@ public class Turner : MonoBehaviour
         _startPosition = Input.mousePosition;
 
         Debug.Log("MouseDown");
+        _distance = 0;
     }
 
     private void OnMouseDrag()
     {
-        Vector3 direction = Input.mousePosition - _startPosition;
+        Vector3 direction = (Input.mousePosition - _startPosition).normalized;
         _myMaterial.SetVector("_Direction", direction);
+        
+        Vector3 corner = _corners[0];
+        float maxDot = Vector3.Dot(direction, transform.position - corner);
+
+        for (int i = 1; i < _corners.Length; i++)
+        {
+            float dot = Vector3.Dot(direction, transform.position - _corners[i]);
+            if(dot > maxDot)
+            {
+                maxDot = dot;
+                corner = _corners[i];
+            }
+        }
+
+        _distance += _deltaDistance;
+        _myMaterial.SetVector("_Position", corner + direction * _distance);
     }
 }
